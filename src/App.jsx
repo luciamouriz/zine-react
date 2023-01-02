bf59e9949bfe6834c91bf9cc684c3a32932cfebb
Lucia <luciamouriz91@gmail.com>
2023-01-02 13:03:09 +0100

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import { Home } from './pages/Home'
 import { InfoVideo } from './pages/InfoVideo'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
     return (
 
         
             <Routes>
-                <Route exact element={<Home />} path="/" />
+                <Route element={<Home />} path="/" />
                 <Route element={<InfoVideo />} path=":video/:id" />
             </Routes>
         
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import "./styles/style.scss";
 import "./assets/fonts/stylefonts.css";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
