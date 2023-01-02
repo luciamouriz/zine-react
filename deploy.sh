a4edf7e8e14fdbc561b296f785db88fddb79799c
Lucia <luciamouriz91@gmail.com>
2023-01-02 12:44:13 +0100

    route react gh-pages

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:luciamouriz/zine-react.git main:gh-pages
+if you are deploying to https://luciamouriz.github.io/zine-react
+git push -f git@github.com:luciamouriz/zine-react.git main:gh-pages
 
 cd -
