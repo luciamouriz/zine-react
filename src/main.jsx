177ddeeae7cbad257d8bb013fbd5bf21398927da
Lucia <luciamouriz91@gmail.com>
2023-01-02 11:48:44 +0100

    gh-pages vite pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import { App } from './App'
 import "./styles/style.scss";
 import "./assets/fonts/stylefonts.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
